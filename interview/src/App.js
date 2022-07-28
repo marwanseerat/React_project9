0f6caa65e2679d63040b7310c1c606f291677aff
marwanseerat <marwanseerat06@gmail.com>
2022-07-28 22:25:11 +0300

    add template to react project and make route

--- interview/src/App.js
+++ interview/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes,BrowserRouter, Route,Link} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './component/Footer';
+import Navbar from './component/Navbar';
+import About from './About'
+import Contact from './Contact'
+import Courses from './Courses'
+import Home from './Home';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/About" element={<About />}/>
+        <Route path="/Contact" element={<Contact />}/>
+        <Route path="/Courses" element={<Courses />}/>
+      </Routes>
+    </BrowserRouter>
+    <Footer/>
+      
     </div>
   );
 }
